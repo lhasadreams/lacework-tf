ed73f1f2b00614340e3a10e0582a6e2d024b9a8c
Adrian Daniels <adrian.daniels@me.com>
2023-06-28 09:32:26 +0100

    commit

--- main.tf
+++ main.tf
@@ -20,16 +20,17 @@ provider "lacework" {
   api_secret = var.lacework_api-secret
 }
 
-module "aws_config" {
-  source  = "lacework/config/aws"
-  version = "~> 0.5"
-}
+# module "aws_config" {
+#   source  = "lacework/config/aws"
+#   version = "~> 0.5"
+#   count=0
+# }
 
-module "main_cloudtrail" {
-  source                = "lacework/cloudtrail/aws"
-  version               = "~> 2.0"
-  iam_role_arn          = module.aws_config.iam_role_arn
-  iam_role_external_id  = module.aws_config.external_id
-  iam_role_name         = module.aws_config.iam_role_name
-  use_existing_iam_role = true
-}
+# module "main_cloudtrail" {
+#   source                = "lacework/cloudtrail/aws"
+#   version               = "~> 2.0"
+#   iam_role_arn          = module.aws_config.iam_role_arn
+#   iam_role_external_id  = module.aws_config.external_id
+#   iam_role_name         = module.aws_config.iam_role_name
+#   use_existing_iam_role = true
+# }
